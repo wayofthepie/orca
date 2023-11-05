b286eeb20e67b38d1e0732303b654d1487e9d5f5
wayofthepie <wayofthepie@gmail.com>
2023-11-05 22:51:19 +0000

    Add work on wait

--- src/main.rs
+++ src/main.rs
@@ -126,12 +126,13 @@ impl OrcaHCloud {
         &self,
         server_id: i64,
         timeout: Duration,
+        tick: Duration,
     ) -> Result<(), Box<dyn std::error::Error>> {
         let mut response = self.get_server(server_id).await?;
         let now = Instant::now();
         while response.server.unwrap().status != Status::Running {
             response = self.get_server(server_id).await?;
-            tokio::time::sleep(Duration::from_secs(5)).await;
+            tokio::time::sleep(tick).await;
             if now.elapsed() > timeout {
                 return Err(format!("Timeout {:?} reached!", timeout).into());
             }
@@ -142,17 +143,22 @@ impl OrcaHCloud {
 
 #[cfg(test)]
 mod test {
+    use std::time::Duration;
+
     use async_trait::async_trait;
     use hcloud::{
         apis::servers_api::{
             CreateServerError, CreateServerParams, GetServerError, GetServerParams,
         },
-        models::{CreateServerResponse, GetServerResponse},
+        models::{server::Status, CreateServerResponse, GetServerResponse, Server},
     };
 
     use crate::{HCloud, HCloudLocation, HCloudServerType, OrcaHCloud};
 
-    struct FakeClient {}
+    #[derive(Default)]
+    struct FakeClient {
+        get_server_response: Option<GetServerResponse>,
+    }
 
     #[async_trait]
     impl HCloud for FakeClient {
@@ -166,13 +172,13 @@ mod test {
             &self,
             _: GetServerParams,
         ) -> Result<GetServerResponse, hcloud::apis::Error<GetServerError>> {
-            Ok(GetServerResponse::default())
+            Ok(self.get_server_response.clone().unwrap_or_default())
         }
     }
 
     #[tokio::test]
     async fn orca_create_server_should_be_ok() {
-        let hcloud = Box::new(FakeClient {});
+        let hcloud = Box::<FakeClient>::default();
         let orca_hcloud = OrcaHCloud { hcloud };
         let response = orca_hcloud
             .create_server(
@@ -187,9 +193,27 @@ mod test {
 
     #[tokio::test]
     async fn orca_get_server_should_be_ok() {
-        let hcloud = Box::new(FakeClient {});
+        let hcloud = Box::<FakeClient>::default();
         let orca_hcloud = OrcaHCloud { hcloud };
         let response = orca_hcloud.get_server(12345).await;
         assert!(response.is_ok());
+    }
+
+    #[tokio::test]
+    async fn orca_wait_until_running_should_timeout_after_given_time() {
+        let hcloud = Box::new(FakeClient {
+            get_server_response: Some(GetServerResponse {
+                server: Some(Box::new(Server {
+                    status: Status::Initializing,
+                    ..Server::default()
+                })),
+            }),
+        });
+        let orca_hcloud = OrcaHCloud { hcloud };
+        let response = orca_hcloud
+            .wait_until_running(12345, Duration::from_millis(10), Duration::from_millis(1))
+            .await;
+        assert!(response.is_err());
+        assert_eq!(response.unwrap_err().to_string(), "Timeout 10ms reached!");
     }
 }
